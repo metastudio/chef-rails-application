ee85814c5083440f006381de8b8ff731f7bda9e9
Andrey Chernih <andrey.chernih@gmail.com>
2013-05-12 21:50:40 +0400

    Update lwrp names

--- recipes/default.rb
+++ recipes/default.rb
@@ -4,14 +4,14 @@ include_recipe "#{cookbook_name}::config"
 
 # We need to generate SSH key for application account in order to add
 # it to the SCM
-ssh_generator node[:application][:user][:name]
+chef_rails_ssh_generator node[:application][:user][:name]
 
 # We need to add keys to authorized_keys in order to be able
 # to deploy without password
 
 all_keys = search(:authorized_keys, '*:*').to_a
 all_keys.select! { |key| node[:authorization][:ssh].include?(key['id']) } if node[:authorization][:ssh]
-ssh_authorizer node[:application][:user][:name] do
+chef_rails_ssh_authorizer node[:application][:user][:name] do
   keys all_keys.map{|data_bag_item| data_bag_item['key']}
 end
 
